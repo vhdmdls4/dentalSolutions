ebba697c55d7e3becb629f82e2ca109254a818f0
Henrique Fantini <112645969+HenriqueFantini@users.noreply.github.com>
2023-09-17 11:07:07 -0300

    Update class.Profissional.php main

--- src/class.Profissional.php
+++ src/class.Profissional.php
@@ -1,13 +1,25 @@
 <?php
 
 require_once ("class.Pessoa.php");
+require_once ("class.Endereco.php");
 
   
 abstract class Profissional extends Pessoa 
 {
 
-  public function __construct () { }
-  
+  public function __construct ($nome, $telefone, $email, $CPF, Endereco $endereco) {}
+ 
+  public function exibeEndereco(){
+    echo "Rua: " . $this-> rua . "\n";
+    echo "Bairro: " . $this-> bairro . "\n";
+    echo "CEP: " . $this-> cep . "\n";
+    echo "Complemento: " . $this-> complemento . "\n";
+    echo "Numero: " . $this-> numero . "\n";
+    echo "Cidade: " . $this-> cidade . "\n";
+    echo "Estado: " . $this-> estado . "\n";
+  }
+
+
 }
  
 
