0e182856c6743924571caae5442f8b575976a008
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2023-09-17 16:14:26 -0300

    Update class.Auxiliar.php

--- src/class.Auxiliar.php
+++ src/class.Auxiliar.php
@@ -4,8 +4,12 @@ require_once("class.Funcionario.php");
 
 class Auxiliar extends Funcionario
 {
+    
     // Sem atributos especificos para a classe no Sprint 1.
 
+    /* Codigo desnecessario por enquanto ja que a classe nao possui atributos proprios e herda o construtor do pai.
+       Vou deixar para facilitar a cricao do construtor proprio futuramente.
+    
     // Caso o construtor de Profissional receba o objeto Endereco como parametro.
     public function __construct
     (
