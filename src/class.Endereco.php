5c31bf18e65a8ad77d50d3b8d6c320cda1d6a460
Pedro Coura <94083207+courapedro@users.noreply.github.com>
2023-09-17 16:28:35 -0300

    Add class.Endereco.php

---
+++ src/class.Endereco.php
@@ -0,0 +1,85 @@
+<?php 
+
+class Endereco {
+    private $rua;
+    private $bairro;
+    private $cep;
+    private $complemento;
+    private $numero;
+    private $cidade;
+    private $estado;
+
+    public function __construct($rua, $bairro, $cep, $complemento, $numero, $cidade, $estado)
+    {
+        $this->rua = $rua;
+        $this->bairro = $bairro;
+        $this->cep = $cep;
+        $this->complemento = $complemento;
+        $this->numero = $numero;
+        $this->cidade = $cidade;
+        $this->estado = $estado;
+    }
+
+    public function getRua()
+    {
+        return $this->rua;
+    }
+
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function cadastrarEndereco()
+    {
+        echo "Informe os dados do endereço:\n";
+        echo "Rua: ";
+        $this->rua = trim(fgets(STDIN));
+
+        echo "Bairro: ";
+        $this->bairro = trim(fgets(STDIN));
+
+        echo "CEP: ";
+        $this->cep = trim(fgets(STDIN));
+
+        echo "Complemento: ";
+        $this->complemento = trim(fgets(STDIN));
+
+        echo "Número: ";
+        $this->numero = trim(fgets(STDIN));
+
+        echo "Cidade: ";
+        $this->cidade = trim(fgets(STDIN));
+
+        echo "Estado: ";
+        $this->estado = trim(fgets(STDIN));
+    }
+    
+}
+
+?>
