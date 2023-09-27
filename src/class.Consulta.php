b78e19e2614779d1667a28a9182fdc36f5dc860a
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2023-09-27 18:09:55 -0300

    add class.Consulta.php

---
+++ src/class.Consulta.php
@@ -0,0 +1,83 @@
+<?php 
+
+class Consulta
+{
+    private Procedimentos $procedimento;
+    private Paciente $paciente;
+    private Dentista $dentistaExecutor;
+    private DateTime $data;
+    private DateTime $horario;
+    private Int $duracao;
+
+    public function __construct(Procedimentos $procedimento, Paciente $paciente, Dentista $dentistaExecutor, DateTime $data, DateTime $horario, Int $duracao)
+    {
+        $this->procedimento = $procedimento;
+        $this->paciente = $paciente;
+        $this->dentistaExecutor = $dentistaExecutor;
+        $this->data = $data;
+        $this->horario = $horario;
+        $this->duracao = $duracao;
+    }
+
+    public function getProcedimento() : Procedimentos
+    {
+        return $this->procedimento;
+    }
+
+    public function getPaciente() : Paciente
+    {
+        return $this->paciente;
+    }
+
+    public function getDentistaExecutor() : Dentista
+    {
+        return $this->dentistaExecutor;
+    }
+
+    public function getData() : DateTime
+    {
+        return $this->data;
+    }
+
+    public function getHorario() : DateTime
+    {
+        return $this->horario;
+    }
+
+    public function getDuracao() : Int
+    {
+        return $this->duracao;
+    }
+
+    public function setProcedimento(Procedimentos $procedimento)
+    {
+        $this->procedimento = $procedimento;
+    }
+
+    public function setPaciente(Paciente $paciente)
+    {
+        $this->paciente = $paciente;
+    }
+
+    public function setDentistaExecutor(Dentista $dentistaExecutor)
+    {
+        $this->dentistaExecutor = $dentistaExecutor;
+    }
+
+    public function setData(DateTime $data)
+    {
+        $this->data = $data;
+    }
+
+    public function setHorario(DateTime $horario)
+    {
+        $this->horario = $horario;
+    }
+
+    public function setDuracao(Int $duracao)
+    {
+        $this->duracao = $duracao;
+    }
+}
+
+?>
