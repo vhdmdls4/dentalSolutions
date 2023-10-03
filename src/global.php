fcc012e291d31618f434eac59b71b6e7d82bbf39
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2023-10-03 16:01:55 -0300

    add global.php

---
+++ src/global.php
@@ -0,0 +1,16 @@
+<?php
+function autoloader($pClassName)
+{
+    //echo __NAMESPACE__;
+    $path = __DIR__ . $pClassName . '.php';
+    if (is_file($path)) {
+        include_once $path;
+    } else {
+        $path = __DIR__ . '/class.' . $pClassName . '.php';
+        if (is_file($path)) {
+            include_once $path;
+        } else
+            throw (new Exception('Não foi encontrada a definição da classe ' . $pClassName . ' na pasta classes.'));
+    }
+}
+spl_autoload_register('autoloader');
