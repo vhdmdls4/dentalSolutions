9bc1461d0f40cda620a2cfbeb8a902b754849845
Henrique Fantini <112645969+HenriqueFantini@users.noreply.github.com>
2023-11-02 13:12:44 -0300

    Update main.php

--- src/main.php
+++ src/main.php
@@ -2,6 +2,7 @@
 
 require_once('../global.php');
 
+// Criação das classes
 
 $end = new Endereco("Antonio Carlos", "Sao Luiz", "12312123", "Portaria 1", 123, "BH", "MG");
 
@@ -26,8 +27,6 @@ $paciente = new Paciente();
 
 $pagamento = new Pagamento ();
 
-$pessoa = new Pessoa();
-
 $procedimento = new Procedimentos();
 
 $profissional = new Profissional();
@@ -35,3 +34,35 @@ $profissional = new Profissional();
 $secretario = new Secretario("Henrique", "31999999999", "secrt@dentalsolutions.com.br", "32132132132", $end, 4321,43);
 
 */
+
+
+
+//Teste do login de um único Usuario
+$usuario = new Usuario("Henrique");
+
+$usuario2 = new Usuario("Ramon");
+
+$login = Login::getInstance();
+$login->logar($usuario);
+
+var_dump($login);
+
+echo "  ||||||||||||||||||||||||||||||||||  "; //separador pra identificar melhor a impressao na tela de cada login
+
+$login2 = Login::getInstance();
+$login2->logar($usuario2);
+
+var_dump($login2);
+
+echo "||||||||||||||||||||||||||||||||||"; // ^^
+
+if($login === $login2){ // se $login for EXATAMENTE igual a $login2 (isso inclui mesmo endereço)
+
+    echo "São iguais!\n"; // se isso for impresso, o login unico funcionou 
+
+}
+
+$login2->deslogar();
+
+var_dump($login); // necessário retornar Null, após $login2 ter sido deslogado
+
