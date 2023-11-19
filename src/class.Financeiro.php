125927a40aba0784698d039b52b20a3b4165ae28
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2023-11-19 17:13:26 -0300

    Fix Financeiro

--- src/class.Financeiro.php
+++ src/class.Financeiro.php
@@ -36,30 +36,11 @@ class Financeiro
         $gastos = $this->calculaGastosSalario($mes);
 
         foreach ($pagamentos as $pagamento) {
-            if ($pagamento->getData()->format('y-m') == $mes->format('y-m')) {
+            if ($pagamento->getData()->format('y-m') == $mes->format('y-m') && $pagamento->getPago()) {
                 $receita += $pagamento->calculaReceita();
             }
         }
 
         return $receita - $gastos;
-    }
-
-    public function calculaRendaPeriodo(DateTime $inicio, DateTime $fim): float
-    {
-        $parceiros = DentistaParceiro::getRecords();
-        $pagamentos = Pagamento::getRecords();
-        $renda = 0.0;
-
-        foreach ($pagamentos as $pagamento) {
-            if ($pagamento->getData() >= $inicio && $pagamento->getData() <= $fim) {
-                $renda += $pagamento->calculaReceita();
-            }
-        }
-
-        foreach ($parceiros as $parceiro) {
-            $renda -= $parceiro->getRenda($inicio, $fim);
-        }
-
-        return $renda;
     }
 }
