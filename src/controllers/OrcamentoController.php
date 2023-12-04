d39d6d55929f3e25f8b3209703d08ce4aaf7475c
courapedro <p_coura@icloud.com>
2023-12-04 14:46:32 -0300

    Atualizacao Orcamento

--- src/controllers/OrcamentoController.php
+++ src/controllers/OrcamentoController.php
@@ -2,33 +2,33 @@
 
 require_once '../global.php';
 
-$formaPagamento1 = new FormaPagamento(TipoPagamento::Credito, 10, 0.05);
+/*$formaPagamento1 = new FormaPagamento(TipoPagamento::Credito, 10, 0.05);
 $pagamento1 = new Pagamento($formaPagamento1, false, new DateTime("2023-11-18"), 1000.00);
-var_dump ($formaPagamento1->getTipo());
+$formaPagamento1->save();
 echo '<hr>';
 echo $pagamento1->getForma();
 echo '<hr>';
 
 $formaPagamento2 = new FormaPagamento(TipoPagamento::Dinheiro, 1, 0.0);
 $pagamento2 = new Pagamento($formaPagamento2, false, new DateTime("2023-11-18"), 1000.00);
-var_dump ($formaPagamento2->getTipo());
+$formaPagamento2->save();
 echo '<hr>';
 echo $pagamento2->getForma();
 echo '<hr>';
 
 $formaPagamento3 = new FormaPagamento(TipoPagamento::Pix, 1, 0.0);
 $pagamento3 = new Pagamento($formaPagamento3, false, new DateTime("2023-11-18"), 500.00);
-var_dump ($formaPagamento3->getTipo());
+$formaPagamento3->save();
 echo '<hr>';
 echo $pagamento3->getForma();
 echo '<hr>';
 
-$formaPagamento3 = new FormaPagamento(TipoPagamento::Debito, 1, 0.01);
+$formaPagamento4 = new FormaPagamento(TipoPagamento::Debito, 1, 0.01);
 $pagamento3 = new Pagamento($formaPagamento3, false, new DateTime("2023-11-18"), 500.00);
-var_dump ($formaPagamento3->getTipo());
+$formaPagamento4->save();
 echo '<hr>';
 echo $pagamento3->getForma();
-echo '<hr>';
+echo '<hr>';*/
 
 class OrcamentoController
 {
@@ -38,18 +38,19 @@ class OrcamentoController
         $pacientesDB = Paciente::getRecords();
         $dentistasDB = DentistaParceiro::getRecords();
         $formasPagamentoDB = FormaPagamento::getRecords();
+        $procedimentosDB = Procedimento::getRecords();
 
         $pacienteCPF = $_POST['pacienteCPF'];
         $dentistaResponsavelCPF = $_POST['dentistaCPF'];
         $dataOrcamento = $_POST['dataOrcamento'];
         $tratamentoAprovado = $_POST['tratamentoAprovado'];
-        $procedimentosData = isset($_POST['procedimentos']) ? filter_input(INPUT_POST, 'procedimentos', FILTER_DEFAULT) : null;
+        $procedimentos = $_POST['procedimentos'];
         $valorTotal = $_POST['valorTotal'];
-        $forma_Pagamento = $_POST['pagamento'];
+        $forma_Pagamento = $_POST['formaPagamento'];
         $descricao = $_POST['descricao'];
         $consultasData = $_POST['consultas'];
 
-        if ($procedimentosData === null) {
+        if ($procedimentos === null) {
             echo json_encode(['error' => 'O campo "procedimentos" não foi enviado ou está vazio.']);
             exit;
         }
@@ -69,7 +70,7 @@ class OrcamentoController
             $dentistaResponsavelCPF === null ||
             $dataOrcamento === null ||
             $tratamentoAprovado === null ||
-            $procedimentosData === null ||
+            $procedimentos === null ||
             $valorTotal === null ||
             $forma_Pagamento === null ||
             $descricao === null ||
@@ -100,11 +101,30 @@ class OrcamentoController
         $formaPagamentoEncontrada = null;
 
         foreach ($formasPagamentoDB as $formaPagamentoLocal) {
-            if ($formaPagamentoLocal->getForma() == $forma_Pagamento) {
+            if ($formaPagamentoLocal->getTipo() == $forma_Pagamento) {
                 $formaPagamentoEncontrada = $formaPagamentoLocal;
                   break;
                 }
         }
+
+        $procedimentoEncontrado = null ;
+
+        foreach ($procedimentosDB as $procedimentoLocal) {
+            if ($procedimentoLocal->getNome() == $procedimentos) {
+                $procedimentoEncontrado = $procedimentoLocal;
+                  break;
+                }
+        }
+
+        /*echo $pacienteCPF ."\n";
+        echo $dentistaResponsavelCPF ."\n";
+        echo $dataOrcamento ."\n";
+        echo $tratamentoAprovado ."\n";
+        echo $procedimentos ."\n"; 
+        echo $valorTotal ."\n";
+        echo $forma_Pagamento."\n";
+        echo $descricao."\n";
+        echo $consultasData."\n";*/
 
         try {
 
@@ -116,8 +136,12 @@ class OrcamentoController
                 throw new Exception('Dentista Responsável não encontrado.');
             }
 
-            if ($formaPagamentoEncontrada === null) {
-                throw new Exception('Forma de Pagamento não encontrada.');
+            if ($formaPagamentoLocal === null) {
+                throw new Exception('Forma de Pagamento '. $formaPagamentoEncontrada . ' não encontrada.');
+            }
+
+            if ($procedimentoLocal === null) {
+                throw new Exception('Procedimento não encontrado.');
             }
 
             /*$pagamento = new Pagamento(
@@ -125,34 +149,36 @@ class OrcamentoController
                 $pagamentoData['pago'],
                 new DateTime($pagamentoData['data']),
                 $pagamentoData['valorFaturado']
-            );*/
+            )*/
+
+            var_dump($procedimentos);
 
             $orcamento = new Orcamento(
-                $pacienteCPF,
-                $dentistaResponsavelCPF,
-                $dataOrcamento,
+                $pacienteEncontrado,
+                $dentistaResponsavelEncontrado,
+            new DateTime($dataOrcamento),
                 $tratamentoAprovado,
-                $procedimentosData,
+                $procedimentos,
                 $valorTotal,
-                $forma_Pagamento,
+                $formaPagamentoLocal,
                 $descricao,
                 $consultasData
             );
             $orcamento->save();
 
-            $orcamentoDetails = [
+            /*$orcamentoDetails = [
                 'paciente' => (string)$orcamento->getPaciente(),
                 'dentistaResponsavel' => (string)$orcamento->getDentistaResponsavel(),
                 'dataOrcamento' => $orcamento->getDataOrcamento()->format('Y-m-d H:i:s'),
                 'tratamentoAprovado' => $orcamento->getTratamentoAprovado(),
-                'procedimentos' => $procedimentosData,
+                'procedimentos' => $procedimentos,
                 'valorTotal' => $orcamento->getValorTotal(),
                 'pagamento' => (string)$orcamento->getPagamento(),
                 'descricao' => $orcamento->getDescricao(),
                 'consultas' => $orcamento->getConsultas(),
             ];
             
-            echo json_encode(['titulo' => 'Orçamento criado com sucesso', 'conteudo' => htmlspecialchars(json_encode($orcamentoDetails))]);
+            echo json_encode(['titulo' => 'Orçamento criado com sucesso', 'conteudo' => htmlspecialchars(json_encode($orcamentoDetails))]);*/
         } catch (Exception $e) {
             echo json_encode(['error' => 'Erro ao criar orçamento: ' . $e->getMessage()]);
         }
